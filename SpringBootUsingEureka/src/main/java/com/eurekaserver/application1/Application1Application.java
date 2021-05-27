9653d9bf8396fc3059ec4204d0166ede3b394a62
PoojaReddy0910 <83747679+PoojaReddy0910@users.noreply.github.com>
2021-05-27 14:25:42 +0530

    Add files via upload

---
+++ SpringBootUsingEureka/src/main/java/com/eurekaserver/application1/Application1Application.java
@@ -0,0 +1,15 @@
+package com.eurekaserver.application1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Application1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application1Application.class, args);
+	}
+
+}
